992336bd0d04ccf41729141a317dcbfc09b8aaf0
examplerweb <exalusproject@gmail.com>
2026-01-20 15:28:35 -0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'virtual-pet-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ASSETS.map(url => new Request(url, {cache: 'reload'}))).catch(err => {
+                    console.log('Failed to cache some assets:', err);
+                });
+            })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then(response => {
+                    return caches.open(CACHE_NAME).then(cache => {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    });
+                });
+            })
+            .catch(() => {
+                return new Response('Offline - No cached data available');
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
